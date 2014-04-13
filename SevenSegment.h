f0088ec81f110ffcf10e81149d6c51000d7d3323
Ob Okaiwele <obokaiwele@gmail.com>
2014-04-13 12:34:44 -0400

    REplacing tabs with spaces

--- SevenSegment.h
+++ SevenSegment.h
@@ -1,39 +1,38 @@
-/*
- * Author:	Obeahon Okaiwele
- * Date: 	September 29, 2012 
+/**
+ * Author:  Obeahon Okaiwele
+ * Date:    September 29, 2012 
  * Purpose: A library to control a 7-segment display
  *
- * Edited:		November 06, 2012 by Obeahon Okaiwele
- * Purpose:		Added _sevenSeg[15][7] and convert()
- *				Added _isAnode and setIsAnode()	
+ * Edited:  November 06, 2012 by Obeahon Okaiwele
+ * Purpose: Added _sevenSeg[15][7] and convert()
+ *          Added _isAnode and setIsAnode()    
  */
 #ifndef SevenSegment_h
 #define SevenSegment_h
 
 #include "Arduino.h"
 
-class SevenSegment
-{
+class SevenSegment {
   public:
     SevenSegment(int pins[7]);
     void show(int num);
     int currentValue();
     void reverseCountDirection();
     int next();
-	void setValue(int value);
-	int getValue();
-	void setLimits(int lowerLimit, int upperLimit);
-	boolean countDirection();
-	int convert(char num);
-	void setIsAnode(boolean isAnode);
+    void setValue(int value);
+    int getValue();
+    void setLimits(int lowerLimit, int upperLimit);
+    boolean countDirection();
+    int convert(char num);
+    void setIsAnode(boolean isAnode);
 
   private:
-	const static char _sevenSeg[16][7];
+    const static char _sevenSeg[16][7];
     int _pins[7];
     int _value;
-	int _lowerLimit;
-	int _upperLimit;
-	int _isAnode;
+    int _lowerLimit;
+    int _upperLimit;
+    int _isAnode;
     boolean _countDirection;  
 };
 
